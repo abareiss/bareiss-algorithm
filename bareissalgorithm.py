b201e5237174e2fd38ed239ae2e6c53f2007cbf7
Abigail Bareiss <abareiss@uci.edu>
2023-12-03 23:58:38 -0800

    Added unit tests

--- bareissalgorithm.py
+++ bareissalgorithm.py
@@ -7,26 +7,26 @@ def bareissalgorithm(matrix: list[list]) -> float:
     
     sign = 1
 
-    for k in range(n-2):
+    for k in range(n-1):
         if matrix[k][k] == 0:
-            for m in range(k+1, n-1):
+            for m in range(k+1, n):
                 if matrix[m][k] != 0:
                     swap(matrix[m], matrix[k])
                     sign = -sign
                     break
 
             # Since all entries were 0 in column k, the determinant is 0
-            if (m == n):
+            if (m == n-1):
                 return 0
             
 
-        for i in range(k+1, n-1):
-            for j in range(k+1, n-1):
-                matrix[i][j] = matrix[k][k] * matrix[i][j] - matrix[i][k] * matrix[k][i]
+        for i in range(k+1, n):
+            for j in range(k+1, n):
+                matrix[i][j] = matrix[k][k] * matrix[i][j] - matrix[i][k] * matrix[k][j]
                 if k!= 0:
                     matrix[i][j] /= matrix[k-1][k-1]
 
-        return sign*matrix[n-1][n-1]
+    return sign*matrix[n-1][n-1]
 
 
 
--- test_bareissalgorithm.py
+++ test_bareissalgorithm.py
@@ -2,9 +2,25 @@ import unittest
 from bareissalgorithm import bareissalgorithm
 
 class TestBareissAlgorithm(unittest.TestCase):
-    def test_matrix(self):
+    
+    def test_valid_matrix_pos_determinant(self):
+        matrix = [[2, 3, 1],[-1, 4, 2],[3, 2, 5]]
+        determinant = bareissalgorithm(matrix)
+        self.assertEqual(determinant, 51, "incorrect determinant")
+        
+    def test_valid_matrix_neg_determinant(self):
         matrix = [[ 42, 97, 23 ],[ 51, 30, 77 ],[ 33, 7, 66 ]]
         determinant = bareissalgorithm(matrix)
         self.assertEqual(determinant, -34062, "incorrect determinant")
+        
+    def test_empty_matrix(self):
+        matrix = []
+        determinant = bareissalgorithm(matrix)
+        self.assertEqual(determinant, 0, "incorrectly determines empty matrix")
+        
+    def test_zero_matrix(self):
+        matrix = [[0,0],[0,0]]
+        determinant = bareissalgorithm(matrix)
+        self.assertEqual(determinant, 0, "incorrect determinant for zero matrix")
 
 unittest.main()
